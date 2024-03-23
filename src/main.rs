49315164627849843c7879421515f7129305468b
gregtemp <greg.m.debicki@gmail.com>
2024-03-23 18:38:15 -0400

    basic stuff working

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,12 @@ fn main() {
 #[derive(Default)]
 struct MyApp {
     slider_val: f64,
-    // declare graph at the beginning
-    graph: Vec<[f64; 2]>,
+    // declare envelope at the beginning
+    envelope: Vec<[f64; 2]>,
+    env_domain: [f64; 2],
+    env_range: [f64; 2],
     lines: Vec<LineSeg>,
+
 }
 
 struct LineSeg {
@@ -32,11 +35,11 @@ struct LineSeg {
 impl MyApp {
     fn update_lines(&mut self) {
         self.lines.clear();
-        let mut sorted_graph = self.graph.clone();
-        sorted_graph.sort_by(|a, b| a[0].partial_cmp(&b[0]).unwrap());
-        for i in 0..sorted_graph.len()-1 {
-            let point1 = sorted_graph[i];
-            let point2 = sorted_graph[i+1];
+        let mut sorted_envelope = self.envelope.clone();
+        sorted_envelope.sort_by(|a, b| a[0].partial_cmp(&b[0]).unwrap());
+        for i in 0..sorted_envelope.len()-1 {
+            let point1 = sorted_envelope[i];
+            let point2 = sorted_envelope[i+1];
             let slope = (point2[1] - point1[1]) / (point2[0] - point1[0]);
             let offset = point1[1] - slope * point1[0];
             self.lines.push(LineSeg { point1, point2, slope, offset });
@@ -58,47 +61,36 @@ impl eframe::App for MyApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.label("Hello, this is my app bro!");
             // slider here
-            ui.add(egui::Slider::new(&mut self.slider_val, 0.0..=5.0).text("Slider"));
+            ui.add(egui::Slider::new(&mut self.slider_val, 1.0..=40.0).text("Frequency"));
             ui.label(format!("Slider value: {}", self.slider_val));
             
-            
-            self.graph = vec![[0.0, 0.0], [0.1, 1.0], [2.0, 0.5], [4.0, 0.5], [2.0*3.1428, 0.0]];
+            self.env_domain = [0.0, 1.0];
+            self.env_range = [-1.0, 1.0];
+            self.envelope = vec![[0.0, 0.0], [0.1, 1.0], [0.3, 0.5], [0.6, 0.5], [1.0, 0.0]];
             self.update_lines();
             
             let plot = Plot::new("my_plot")
                 .view_aspect(2.0)
                 .allow_drag(false)
                 .allow_scroll(false)
-                .include_x(0.0)
-                .include_x(2.0 * 3.1428)
-                .include_y(-1.0)
-                .include_y(1.0);
+                .include_x(self.env_domain[0])
+                .include_x(self.env_domain[1])
+                .include_y(self.env_range[0])
+                .include_y(self.env_range[1]);
             
             plot.show(ui, |plot_ui| { 
                 
-                let showgraph = self.graph.clone();
-                plot_ui.line(Line::new(PlotPoints::from(showgraph)).name("env"));
+                let envelope = self.envelope.clone(); // cant remember why but you can't use it directly
+                plot_ui.line(Line::new(PlotPoints::from(envelope)).name("env"));
 
                 // sine wave
                 let sin: PlotPoints = (0..1000).map(|i| {
-                    let x = i as f64 * 0.01;
-                    // let gain = self.slider_val as f64;
-                    
-                    [x, { (x*(1.0 + self.slider_val* 2.0)).sin() * self.get_y(x) }]
+                    let x = i as f64 * 0.001 * (self.env_domain[1] - self.env_domain[0]) + self.env_domain[0];
+                    [x, { (x*(self.slider_val)).sin() * self.get_y(x) }]
+
                 }).collect();
                 let sin_line = Line::new(sin);
                 plot_ui.line(sin_line);
-
-                // cos wave
-                let cos: PlotPoints = (0..1000).map(|i| {
-                    let x = i as f64 * 0.01;
-                    let gain = self.slider_val as f64;
-                    [x, {x.cos() * gain*0.5 }.tanh()]
-                }).collect();
-                let cos_line = Line::new(cos);
-                plot_ui.line(cos_line);
-
-
 
 
                 });
